d46fb7a61abe1663d1855f49c32ac1ee9c6449c7
Nedim7050 <najdmejri625@gmail.com>
2026-06-21 22:55:53 +0100

    Reopen TANIT registrations

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -2,5 +2,5 @@
 // true  = show the pause page and block registrations
 // false = show the complete website and registration form
 export const siteConfig = {
-  registrationPaused: true,
+  registrationPaused: false,
 }
